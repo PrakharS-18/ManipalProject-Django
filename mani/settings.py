75088f2ab867d4b02d510e5a47bf84d1715317e3
Prakhar Singh <98813977+PrakharS-18@users.noreply.github.com>
2022-11-09 01:03:05 +0530

    Update settings.py

--- mani/settings.py
+++ mani/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vt!rw8zjgm9=47ltassvz&t(2$=54n5p61mp*dztn4s&gpz#)4'
+SECRET_KEY = '***************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
